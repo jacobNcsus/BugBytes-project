2a73ac80d60b9b10f8c4f3d545e2db74ff053ecf
Alexa <Alexa@Alexander-Laptop>
2021-03-07 19:41:18 -0800

    LETS SEE HOW THIS GOES

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301171208 - This line was added by Jacob Normington.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301689193 - This line was added by Alexander Gunby.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
