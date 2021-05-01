f731c4ca473cbd68dc20920c40700b6a3348c74c
Sikander <sikanderghafary@csus.edu>
2021-04-30 17:03:14 -0700

    Added UI portion for the customer

--- BugBytes Storefront/src/ShoppingCartManager.java
+++ BugBytes Storefront/src/ShoppingCartManager.java
@@ -93,4 +93,54 @@ public class ShoppingCartManager
       }
       in.close();
    }
+   
+   /*public static void select() {
+		
+		Scanner s = new Scanner(System.in);
+
+		System.out.printf("\nPlease select a product to add to cart: ");
+		String selection = s.nextLine();
+		
+		//if (selection == )
+		
+	}
+	
+	public static void menu() {
+		
+		Connector c = new Connector();
+		System.out.printf("%15s%21s%16s\n"
+				+ "-------------------------------------------------------\n"
+						 ,"Product","Price","Quantity");
+		c.read("products");
+		
+	}
+
+	
+	// customer logins in with email and password
+	public static void login() {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.printf("Please fill in the following information to create an account:\n\n");
+
+		System.out.printf("First Name: ");
+		String first = s.nextLine();
+		
+		System.out.printf("\nLast Name: ");
+		String last = s.nextLine();
+		
+		System.out.printf("\nEmail: ");
+		String email = s.nextLine();
+		
+		System.out.printf("\nPhone Number: ");
+		String phone = s.nextLine();
+		
+		System.out.printf("\nPreferred Username: ");
+		String username = s.nextLine();
+
+		System.out.printf("\n\tWelcome %s %s to the BugBytes Shopping Center\n\n "+
+						    "\tPlease look around for anything that interests you!\n\n", first,last);
+		
+	}
+	*/
 }
