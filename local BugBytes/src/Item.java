cebc155c03bce0cec9dcf42e19d0813f9d7d8cf1
Sikander <sikanderghafary@csus.edu>
2021-04-30 17:29:37 -0700

    commmitted

---
+++ local BugBytes/src/Item.java
@@ -0,0 +1,126 @@
+/**
+ * An item of inventory for a store. 
+ *
+ * @author Jacob Normington
+ * @version 3/22/2021
+ */
+public class Item 
+{
+	   private int id; //database product id
+	   private String itemName;
+	   private String category;
+	   private String itemDescription;
+	   private int itemPrice; //in USD cents 
+	   private int itemQuantity;
+
+	   /**
+	    * Default constructor for objects of ItemToPurchase class, 
+	    * sets all fields to 0 or equivalent.
+	    */
+	   public Item()
+	   {
+		   itemName = null;
+		   itemDescription = null;
+		   itemPrice = 0;
+		   itemQuantity = 0;
+	   }
+	   
+	   public Item(String name, String description, int price, int quantity)
+	   {
+	      itemName = name;
+	      itemDescription = description;
+	      itemPrice = price;
+	      itemQuantity = quantity;
+	   }
+	   
+	   public int getProductId()
+	   {
+		   return id; 
+	   }
+
+	   public String getName()
+	   {
+	      return itemName;
+	   }
+	   public void setName(String name)
+	   {
+	      itemName = name;
+	   }
+	   
+	   public String getCategory()
+	   {
+	      return category;
+	   }
+	   public void setCategory(String category)
+	   {
+	      this.category = category;
+	   }
+	   
+	   public String getDescription()
+	   {
+	      return itemDescription;
+	   }
+	   public void setDescription(String description)
+	   {
+	      itemDescription = description;
+	   }
+
+	   /**
+	    * Returns the item's price in USD. 
+	    */
+	   public double getPrice()
+	   {
+	      return itemPrice/100.0;
+	   }
+	   /**
+	    * Changes the price of the item.  
+	    * 
+	    * @param	price	the item's value in USD cents. 
+	    */
+	   public void setPrice(int price)
+	   {
+	      itemPrice = price;
+	   }
+	   
+	   public int getQuantity()
+	   {
+	      return itemQuantity;
+	   }
+	   public void setQuantity(int quantity)
+	   {
+	      itemQuantity = quantity;
+	   }
+	   
+	   /**
+	    * Returns the item price times quantity. 
+	    */
+	   public double getTotalCost()
+	   {
+	      return itemQuantity/100.0*itemPrice;
+	   } 
+	   
+	   /**
+	    * Outputs the item name followed by the quantity, price, and subtotal
+	    */
+	   public void printItemCost()
+	   {
+	      System.out.println(getName() + " " + itemQuantity 
+	         + " @ $" + itemPrice + " = $" + getTotalCost());
+	   }
+	   
+	   /**
+	    * Prints to console the item name and description
+	    */
+	   public void printItemDescription()
+	   {
+	      System.out.println(itemName + ": " + itemDescription); 
+	   }
+	   
+	   /**
+	    * Outputs the item name and description
+	    */
+	   public String toString()
+	   {
+	      return itemQuantity + " " + itemName + " @ " + itemQuantity + ": " + itemDescription;
+	   }
+	}
---
+++ local BugBytes/src/ShoppingCartManager.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+/**
+ * Implements a menu to manipulate a shopping cart via console input
+ *
+ * @author Jacob Normington
+ * @version 2/13/2020
+ */
+public class ShoppingCartManager
+{
+   /**
+    * Begins the manager to create your shopping cart by system input
+    */
+   public static void main(String args[])
+   {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Shopping Cart Manager");
+        System.out.println("...");
+        System.out.println("Please enter your name.");
+        String name = in.nextLine();
+        System.out.println("Customer Name: " + name);
+        System.out.println("...");
+        System.out.println("Please enter today's date.");
+        String date = in.nextLine();
+        System.out.println("Current Date: " + date);
+        System.out.println("...");
+        ShoppingCart cart = new ShoppingCart(name, date);
+        printMenu(cart);
+        
+        in.close();
+   }
+    
+   /**
+    * Outputs a menu of options that can be used to manipulate a
+    * shopping cart
+    *
+    * @param   cart  the shopping cart to be modified
+    * @return        none
+    */
+   public static void printMenu(ShoppingCart cart)
+   {
+      Scanner in = new Scanner(System.in);
+      System.out.println();
+      System.out.println("MENU");
+      System.out.println("a - Add item to cart");
+      System.out.println("d - Remove item from cart");
+      System.out.println("c - Change item quantity");
+      System.out.println("i - Output item's descriptions");
+      System.out.println("o - Output shopping cart");
+      System.out.println("q - Quit");
+      System.out.println("");
+      System.out.println("Choose an option: ");
+      String next = in.nextLine();
+      
+      if(!next.equals( "q")) //quit
+      {
+         if(next.equals("a")) //add an item to cart
+         {
+            System.out.println("ADD ITEM TO CART");
+            System.out.println("Enter the item name:");
+            String name = in.nextLine();
+            System.out.println("Enter the item description:");
+            String description = in.nextLine();
+            System.out.println("Enter the item price:");
+            int price = java.lang.Integer.parseInt(in.next());
+            System.out.println("Enter the item quantity:");
+            int quantity = java.lang.Integer.parseInt(in.next());
+            cart.addItem(new Item(name, description, price, quantity));
+         }
+         if(next.equals("d")) //remove an item from cart
+         {
+            System.out.println("REMOVE ITEM FROM CART");
+            System.out.println("Enter name of item to remove:");
+            cart.removeItem(in.nextLine());
+         }
+         if(next.equals("c")) //change item quantity
+         {
+            System.out.println("CHANGE ITEM QUANTITY");
+            Item item = new Item();
+            System.out.println("Enter the item name:");
+            item.setName(in.nextLine());
+            System.out.println("Enter the new quantity:");
+            item.setQuantity(in.nextInt());
+            cart.modifyItem(item);
+         }
+         if(next.equals("i")) //outputs item's descriptions
+         {
+            System.out.println("OUTPUT ITEM'S DESCRIPTIONS");
+            cart.printDescription();
+         }
+         if(next.equals("o")) //outputs shopping cart
+         {
+            System.out.println("OUTPUT SHOPPING CART");
+            System.out.println();
+            cart.printTotal();
+         }
+         printMenu(cart);
+      }
+      in.close();
+   }
+}
